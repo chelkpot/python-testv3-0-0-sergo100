739f35e6af4e7b827a506d18f69a3ed88262d3fa
Сергей Сергиенко <sergo100@yandex.ru>
2025-10-02 03:36:15 +0500

    решил задачи

---
+++ tasks/task6.py
@@ -0,0 +1,4 @@
+r=int(input())
+radius=2*r
+time=4*r//900
+print(radius,time)
---
+++ tasks/task7.py
@@ -0,0 +1,14 @@
+number=int(input())
+d1 = (number // 1000) % 10
+d2 = (number // 100) % 10
+d3 = (number // 10) % 10
+d4 = number % 10
+
+d1 = (d1 + 7) % 10
+d2 = (d2 + 7) % 10
+d3 = (d3 + 7) % 10
+d4 = (d4 + 7) % 10
+
+d1, d2, d3, d4 = d3,d4,d1,d2
+
+print(d1,d2,d3,d4,sep='')
